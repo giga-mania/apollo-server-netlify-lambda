1d1d4c243ebacef793891943c79dd1ec19779d5a
giguchi <hil.giga@gmail.com>
2022-12-02 23:51:50 +0000

    test: deploy apollo server-netlify functions

---
+++ netlify/functions/index.js
@@ -0,0 +1,17 @@
+const {ApolloServer, gql} = require("apollo-server-lambda")
+
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`
+
+const resolvers = {
+    Query: {
+        hello: () => "Went as expected!"
+    }
+}
+
+const server = new ApolloServer({typeDefs, resolvers, playground: true, introspection: true})
+
+exports.handler = server.createHandler()
